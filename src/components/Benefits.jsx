60508a5dc9a731330bc4fbe16b6f53f07ed45d48
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2026-05-02 21:38:41 -0300

    fix: corrects benefits cards background image

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -15,53 +15,63 @@ const Benefits = () => {
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
-          {benefits.map((item) => (
-            <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
-              key={item.id}
-            >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h5 mb-5">{item.title}</h5>
-                <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
-                  <img
-                    src={item.iconUrl}
-                    width={48}
-                    height={48}
-                    alt={item.title}
-                  />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
-                  <Arrow />
-                </div>
-              </div>
-
-              {item.light && <GradientLight />}
-
+          {benefits.map((item) => {
+            console.log(item.backgroundUrl);
+            return (
               <div
-                className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: "url(#benefits" }}
+                className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                // style={{
+                //   backgroundImage: `url(${item.backgroundUrl})`,
+                // }}
+                key={item.id}
               >
-                <div className="absolute inset-0 opacity-0 transition-opacity duration-300 hover:opacity-10">
-                  {item.imageUrl && (
-                    <img
-                      src={item.imageUrl}
-                      width={380}
-                      height={362}
-                      alt={item.title}
-                      className="w-full h-full object-cover"
-                    />
-                  )}
+                <div className="absolute top-0 left-0 w-full h-full">
+                  <img
+                    className="w-full h-full"
+                    src={item.backgroundUrl}
+                    alt=""
+                  />
                 </div>
-              </div>
+                <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                  <h5 className="h5 mb-5">{item.title}</h5>
+                  <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                  <div className="flex items-center mt-auto">
+                    <img
+                      src={item.iconUrl}
+                      width={48}
+                      height={48}
+                      alt={item.title}
+                    />
+                    <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                      Explore more
+                    </p>
+                    <Arrow />
+                  </div>
+                </div>
 
-              <ClipPath />
-            </div>
-          ))}
+                {item.light && <GradientLight />}
+
+                <div
+                  className="absolute inset-0.5 bg-n-8"
+                  style={{ clipPath: "url(#benefits" }}
+                >
+                  <div className="absolute inset-0 opacity-0 transition-opacity duration-300 hover:opacity-10">
+                    {item.imageUrl && (
+                      <img
+                        src={item.imageUrl}
+                        width={380}
+                        height={362}
+                        alt={item.title}
+                        className="w-full h-full object-cover"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <ClipPath />
+              </div>
+            );
+          })}
         </div>
       </div>
     </Section>
